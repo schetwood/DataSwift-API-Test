c60bcb61d5845fc4cdd3bd960b60dcdf22b1f9ff
Simon Chetwood <simon.chetwood@mkodo.com>
2020-07-04 15:00:09 +0100

    Initial Commit

---
+++ DataSwift-API-Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataSwift-API-Test
+//
+//  Created by Simon Chetwood on 04/07/2020.
+//  Copyright © 2020 DataSwift. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ DataSwift-API-TestTests/DataSwift_API_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  DataSwift_API_TestTests.swift
+//  DataSwift-API-TestTests
+//
+//  Created by Simon Chetwood on 04/07/2020.
+//  Copyright © 2020 DataSwift. All rights reserved.
+//
+
+import XCTest
+@testable import DataSwift_API_Test
+
+class DataSwift_API_TestTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
